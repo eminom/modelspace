c9eb1ad2360460882727f27f3427475827ecfb00
baihai <hai.bai.cn@gmail.com>
2015-04-10 16:47:50 +0800

    update.

--- source/utils/signalsinc.hpp
+++ source/utils/signalsinc.hpp
@@ -27,7 +27,32 @@ namespace DD
 	class Signal;
 };
 //And do some template partialization
+//Four
+#define ARG_SEP				,
+#define ARG_SIGT			typename P1, typename P2, typename P3, typename P4
+#define ARG_SIGV			P1 p1, P2 p2, P3 p3, P4 p4
+#define ARG_SIGB			P1, P2, P3, P4
+#define ARG_SIGS			p1, p2, p3, p4
+#include "signals.hpp"
+#undef ARG_SIGS
+#undef ARG_SIGB
+#undef ARG_SIGV
+#undef ARG_SIGT
+#undef ARG_SEP
 
+
+//Three
+#define ARG_SEP				,
+#define ARG_SIGT			typename P1, typename P2, typename P3
+#define ARG_SIGV			P1 p1, P2 p2, P3 p3
+#define ARG_SIGB			P1, P2, P3
+#define ARG_SIGS			p1, p2, p3
+#include "signals.hpp"
+#undef ARG_SIGS
+#undef ARG_SIGB
+#undef ARG_SIGV
+#undef ARG_SIGT
+#undef ARG_SEP
 //
 //Two
 #define ARG_SEP				,
@@ -42,18 +67,18 @@ namespace DD
 #undef ARG_SIGT
 #undef ARG_SEP
 //
-////Zero
-//#define ARG_SEP		
-//#define ARG_SIGT	
-//#define ARG_SIGV
-//#define ARG_SIGB
-//#define ARG_SIGS
-//#include "signals.hpp"
-//#undef ARG_SIGS
-//#undef ARG_SIGB
-//#undef ARG_SIGV
-//#undef ARG_SIGT
-//#undef ARG_SEP
+//Zero
+#define ARG_SEP		
+#define ARG_SIGT	
+#define ARG_SIGV
+#define ARG_SIGB
+#define ARG_SIGS
+#include "signals.hpp"
+#undef ARG_SIGS
+#undef ARG_SIGB
+#undef ARG_SIGV
+#undef ARG_SIGT
+#undef ARG_SEP
 //
 ////One
 #define ARG_SEP			,
